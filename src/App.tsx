21c2aa886e377886b4ac49ca832fc399ed610159
wangjq <wjqsrc@outlook.com>
2019-04-13 01:01:08 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import COM from './layout';
+
+class App extends Component {
+  render() {
+    return (
+        <COM/>
+    );
+  }
+}
+
+export default App;
