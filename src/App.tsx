0876b7cc6ddb2b7a6c0b094a61b5ef59e53e32ce
wangjq <wjqsrc@outlook.com>
2019-04-13 20:52:49 +0800

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import COM from './layout';
 
 class App extends Component {
-  render() {
-    return (
-        <COM/>
-    );
-  }
+    render() {
+        return (
+            <COM/>
+        );
+    }
 }
 
 export default App;
